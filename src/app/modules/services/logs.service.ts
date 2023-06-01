9f969e56d06b0ee0f1f12d9f7740f45a12a30a58
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2023-06-01 09:22:14 +0200

    inversion des logs

--- src/app/modules/services/logs.service.ts
+++ src/app/modules/services/logs.service.ts
@@ -13,7 +13,7 @@ export class LogsService {
 
   send(log:string) {
     const date:Date = new Date();
-    const logs = [...this._logs, `${date.toLocaleTimeString('fr-FR')} - ${log}`];
+    const logs = [`${date.toLocaleTimeString('fr-FR')} - ${log}`, ...this._logs];
     this._logs = logs.length > 20?logs.slice(-20):logs;
 
     this._logger.next(this._logs);
